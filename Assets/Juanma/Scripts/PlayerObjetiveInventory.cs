0f2c88bcab079735924d9825a39c71e2959962f5
jmparedes7 <87044417+jmparedes7@users.noreply.github.com>
2026-06-05 17:11:31 -0300

    UI BASICA
    Se implemento un sistema basico de ui:
    Contador de recuerdos

--- Assets/Juanma/Scripts/PlayerObjetiveInventory.cs
+++ Assets/Juanma/Scripts/PlayerObjetiveInventory.cs
@@ -1,18 +1,44 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerObjectiveInventory : MonoBehaviour
 {
+    [Header("Objetivos")]
     public int collectedItems = 0;
     public int requiredItems = 3;
+
+    [Header("UI")]
+    public TextMeshProUGUI objectiveText;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
 
     public void AddItem()
     {
         collectedItems++;
+
+        if (collectedItems > requiredItems)
+        {
+            collectedItems = requiredItems;
+        }
+
+        UpdateUI();
+
         Debug.Log("Objetos recolectados: " + collectedItems + "/" + requiredItems);
     }
 
     public bool HasAllItems()
     {
         return collectedItems >= requiredItems;
+    }
+
+    private void UpdateUI()
+    {
+        if (objectiveText != null)
+        {
+            objectiveText.text = "Recuerdos encontrados: " + collectedItems + "/" + requiredItems;
+        }
     }
 }
